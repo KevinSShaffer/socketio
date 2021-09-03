5a3330fa84b8b94c381d4ec552f8ae3b7f1c6775
kevinsshaffer <kevinsshaffer@gmail.com>
2021-09-03 15:23:08 -0600

    Display coordinate onclick

--- static/main.js
+++ static/main.js
@@ -4,14 +4,11 @@ document.addEventListener('keydown', keyDown);
 
 window.addEventListener('load', (event) => {
     const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
 
-    if (canvas.getContext) {
-        var context = canvas.getContext('2d');
-
-        context.fillStyle = 'rgb(200, 0, 0)';
-        context.fillRect(10, 10, 50, 50);
-
-        context.fillStyle = 'rgb(0, 0, 200, 0.5)';
-        context.fillRect(30, 30, 50, 50);
-    }
+    context.font = '12px serif';
+    
+    canvas.addEventListener('mousedown', (event) => {
+        context.fillText(`(${event.clientX}, ${event.clientY})`, event.clientX, event.clientY);
+    });
 });
