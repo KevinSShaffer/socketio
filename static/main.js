36b5be512c78a64c6bae03744f2bafb6d957ad42
kevinsshaffer <kevinsshaffer@gmail.com>
2021-09-03 14:59:05 -0600

    Add canvas element

--- static/main.js
+++ static/main.js
@@ -1,3 +1,17 @@
 import { keyDown } from './modules/handlers.js';
 
 document.addEventListener('keydown', keyDown);
+
+window.addEventListener('load', (event) => {
+    const canvas = document.getElementById('canvas');
+
+    if (canvas.getContext) {
+        var context = canvas.getContext('2d');
+
+        context.fillStyle = 'rgb(200, 0, 0)';
+        context.fillRect(10, 10, 50, 50);
+
+        context.fillStyle = 'rgb(0, 0, 200, 0.5)';
+        context.fillRect(30, 30, 50, 50);
+    }
+});
