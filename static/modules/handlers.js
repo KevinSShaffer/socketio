cba352f10944b4ab38748bf7b1c8e1dbdeb1f9d6
kevinsshaffer <kevinsshaffer@gmail.com>
2021-09-03 13:14:50 -0600

    Remove ArrowDown case

--- static/modules/handlers.js
+++ static/modules/handlers.js
@@ -3,9 +3,6 @@ export function keyDown(event) {
     const keyName = event.key;
 
     switch (keyName) {
-        case 'ArrowDown':
-            
-            break;    
         default:
             console.log(keyName);
             break;
