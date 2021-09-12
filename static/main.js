63a7c00d245b3dd12c4036c5880cf03ca4603c85
kevinsshaffer <kevinsshaffer@gmail.com>
2021-09-11 21:59:47 -0600

    Add game loop

--- static/main.js
+++ static/main.js
@@ -1,41 +1,37 @@
-import { characters, numbers, nonalphaPropers, isValidForProperName } from './modules/text.js';
+import { isValidForProperName } from './modules/text.js';
 
 const canvasPaddingInPixels = 10;
 const socket = io();
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
 
-let canvas = {};
-let context = {};
+let lastAnimationFrameId = 0;
 
 let nameText = {
     x: 50, 
     y: 50,
     fontSize: 12,
     text: '',
+    get font() {
+        return `bold ${this.fontSize}px monospace`;
+    },
     get position() { 
         return this.x + (.75 * this.fontSize * this.text.length); 
     }
 };
 
 function windowOnLoad(event) {
-    canvas = document.getElementById('canvas');
-    context = canvas.getContext('2d');
-
     resizeCanvas();
     
     canvas.addEventListener('mousedown', (event) => {
         const coordinates = { x: event.offsetX, y: event.offsetY };
-
-        context.fillText(`(${coordinates.x}, ${coordinates.y})`, coordinates.x, coordinates.y);
-
-        socket.emit('mousedown', coordinates);
     });
 }
 
 function resizeCanvas() {
     canvas.setAttribute('width', window.innerWidth - (canvasPaddingInPixels * 2));
     canvas.setAttribute('height', window.innerWidth * 3 / 4 - (canvasPaddingInPixels * 2));
-    canvas.setAttribute('padding', `${canvasPaddingInPixels}px`)
-    context.font = `bold ${nameText.fontSize}px monospace`;
+    canvas.setAttribute('padding', `${canvasPaddingInPixels}px`);
 }
 
 function onKeyDown(event) {
@@ -58,6 +54,20 @@ window.onload = windowOnLoad;
 window.onresize = resizeCanvas;
 document.onkeydown = onKeyDown;
 
-socket.on('mousedown', (coordinates) => {    
-    context.fillText(`(${coordinates.x}, ${coordinates.y})`, coordinates.x, coordinates.y);
-});
+;(function () {
+    function  main() {
+        lastAnimationFrameId = window.requestAnimationFrame(main);
+
+        context.fillStyle = 'green';
+        context.fillRect(0, 0, canvas.getAttribute('width'), canvas.getAttribute('height'));
+
+        context.fillStyle = 'white';
+        context.font = nameText.font;
+        context.fillRect(40, 40, canvas.getAttribute('width') - 80, nameText.fontSize * 1.5);
+
+        context.fillStyle = 'black';
+        context.fillText(nameText.text, nameText.x, 40 + nameText.fontSize);
+    }
+
+    main();
+})();
