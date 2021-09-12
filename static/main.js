f49f1f8d3379761a6f6381f13d5d932f65631368
kevinsshaffer <kevinsshaffer@gmail.com>
2021-09-11 19:06:52 -0600

    Typing POC

--- static/main.js
+++ static/main.js
@@ -1,14 +1,15 @@
 import { keyDown } from './modules/handlers.js';
 
 const canvasPaddingInPixels = 10;
+const fontSize = 12
 const socket = io();
 
 let canvas = {};
 let context = {};
 
-document.addEventListener('keydown', keyDown);
+let textPosition = { x: 50, y: 50 };
 
-window.addEventListener('load', (event) => {
+function windowOnLoad(event) {
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
 
@@ -21,17 +22,27 @@ window.addEventListener('load', (event) => {
 
         socket.emit('mousedown', coordinates);
     });
-});
-
-window.onresize = resizeCanvas;
-
-socket.on('mousedown', (coordinates) => {    
-    context.fillText(`(${coordinates.x}, ${coordinates.y})`, coordinates.x, coordinates.y);
-});
+}
 
 function resizeCanvas() {
     canvas.setAttribute('width', window.innerWidth - (canvasPaddingInPixels * 2));
     canvas.setAttribute('height', window.innerWidth * 3 / 4 - (canvasPaddingInPixels * 2));
     canvas.setAttribute('padding', `${canvasPaddingInPixels}px`)
-    context.font = '12px serif';
+    context.font = `bold ${fontSize}px monospace`;
 }
+
+function onKeyDown(event) {
+    const keyName = event.key;
+
+    context.fillText(keyName, textPosition.x, textPosition.y);
+
+    textPosition.x += .75 * fontSize;
+}
+
+window.onload = windowOnLoad;
+window.onresize = resizeCanvas;
+document.onkeydown = onKeyDown;
+
+socket.on('mousedown', (coordinates) => {    
+    context.fillText(`(${coordinates.x}, ${coordinates.y})`, coordinates.x, coordinates.y);
+});
