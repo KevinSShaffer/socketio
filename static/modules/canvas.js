056cd40c305ef2fdae9aac817d7d4799f222ea34
kevinsshaffer <kevinsshaffer@gmail.com>
2021-09-12 16:51:32 -0600

    Element focus onclick

--- static/modules/canvas.js
+++ static/modules/canvas.js
@@ -18,18 +18,43 @@ export class Canvas {
         this.#context = this.#canvas.getContext('2d');
         this.padding = 10;
         window.onresize = this.resize;
-        this.#canvas.onclick
+        this.#canvas.onclick = this.onclick;
         this.resize();
+        this.backgroundColor = 'green';
     }
 
     resize = () => {
-        this.#canvas.setAttribute('width', window.innerWidth - (this.padding * 2));
-        this.#canvas.setAttribute('height', window.innerWidth * 3 / 4 - (this.padding * 2));
-        this.#canvas.setAttribute('padding', `${this.padding}px`);
+        // height = 3/4 width is 4:3 aspect ratio
+        this.#canvas.width = window.innerWidth - (this.padding * 2);
+        this.#canvas.height = window.innerWidth * 3 / 4 - (this.padding * 2);
+        this.#canvas.padding = `${this.padding}px`;
     }
 
-    render() {
-        this.#context.fillStyle = 'green';
+    onclick = (event) => {
+        for (let i = this.#uiElements.length - 1; i >= 0; i--) {
+            const element = this.#uiElements[i];
+            const [y, x] = [event.clientY, event.clientX];
+
+            if (element.isInside(x, y)) {
+                element.onclick(event);
+
+                // remove focus from top element
+                this.#uiElements[this.#uiElements.length - 1].focus(false);
+
+                // move clicked element to top
+                this.#uiElements.splice(i, 1);
+                this.#uiElements.push(element);
+
+                // focus top element
+                this.#uiElements[this.#uiElements.length - 1].focus(true);
+
+                break;
+            }
+        }
+    }
+
+    render = () => {
+        this.#context.fillStyle = this.backgroundColor;
         this.#context.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
 
         this.#uiElements.forEach(element => {
--- static/modules/components.js
+++ static/modules/components.js
@@ -1,21 +1,39 @@
 import { isValidForProperName } from './text.js';
 
-export class UsernameModal { 
+export class UsernameModal {
+    #controls = [];
     // constructor could take an "options" object that includes anchors and relative positioning
     constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.textbox = new Textbox(x + 15, y + 20, 30, 12);        
-        this.windowBorderColor = 'darkgrey';
+        this.textbox = new Textbox(x + 15, y + 20, 30, 12);
+        this.#controls.push(this.textbox);
+        this.unfocusedBorderColor = 'darkgrey';
+        this.focusedBorderColor = '#5D6D7E';
+        this.windowBorderColor = this.unfocusedBorderColor;
         this.windowColor = 'lightgrey';
         this.formBorder = 3;
+    }
+
+    focus(isFocused) {
+        if (isFocused) {
+            this.windowBorderColor = this.focusedBorderColor;
+        } else {
+            this.windowBorderColor = this.unfocusedBorderColor;
+        }
     }
 
     onkeydown = (event) => {
         this.textbox.onkeydown(event);
     };
+
+    onclick = (event) => {
+    };
+
+    isInside = (x, y) => y > this.y && y < this.y + this.height &&
+    x > this.x && x < this.x + this.width;
 
     render(context) {
         context.fillStyle = this.windowColor;
