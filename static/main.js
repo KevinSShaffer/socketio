0b255e3cdfc3262068ccfa7b3729720bfb11fb0f
kevinsshaffer <kevinsshaffer@gmail.com>
2021-09-11 18:31:54 -0600

    Canvas sizing

--- static/main.js
+++ static/main.js
@@ -1,17 +1,21 @@
 import { keyDown } from './modules/handlers.js';
 
+const canvasPaddingInPixels = 10;
 const socket = io();
+
+let canvas = {};
+let context = {};
 
 document.addEventListener('keydown', keyDown);
 
 window.addEventListener('load', (event) => {
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
 
-    context.font = '12px serif';
+    resizeCanvas();
     
     canvas.addEventListener('mousedown', (event) => {
-        const coordinates = { x: event.clientX, y: event.clientY };
+        const coordinates = { x: event.offsetX, y: event.offsetY };
 
         context.fillText(`(${coordinates.x}, ${coordinates.y})`, coordinates.x, coordinates.y);
 
@@ -19,9 +23,15 @@ window.addEventListener('load', (event) => {
     });
 });
 
-socket.on('mousedown', (coordinates) => {
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
-    
+window.onresize = resizeCanvas;
+
+socket.on('mousedown', (coordinates) => {    
     context.fillText(`(${coordinates.x}, ${coordinates.y})`, coordinates.x, coordinates.y);
 });
+
+function resizeCanvas() {
+    canvas.setAttribute('width', window.innerWidth - (canvasPaddingInPixels * 2));
+    canvas.setAttribute('height', window.innerWidth * 3 / 4 - (canvasPaddingInPixels * 2));
+    canvas.setAttribute('padding', `${canvasPaddingInPixels}px`)
+    context.font = '12px serif';
+}
